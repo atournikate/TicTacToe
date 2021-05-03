2a02cf9d1c0f1e396e40c8837a4465bd0e6b4429
atournikate <kate.ebensteiner@gmail.com>
2021-05-03 09:19:02 +0200

    Monday edits

--- ttt_game_kate.js
+++ ttt_game_kate.js
@@ -1,8 +1,8 @@
-alert("I am reading this file...");
+alert("I am reading this file..."); //Test JS
 
 var button = document.querySelectorAll(".btn");
 
-alert("There are " + button.length + " buttons");
+alert("There are " + button.length + " buttons"); //Test JS & HTML communication
 
 var winAlert = document.getElementById("textline");
 var p1Win = "Player 1 Wins!"
@@ -12,6 +12,15 @@ var aTie = "It's a Cat's Game!"
 var playerTurn = 0;
 var SYMBOLS = ["X" , "O"];
 // Setting DOM to all buttons
+
+for (i = 0; i <= 1; i++) {
+    if (i > 1) {
+        i = 0+}
+    }
+    button.innerHTML = SYMBOLS[i];
+    }
+    } 
+};
 
 function myGame() {
     var b1, b2, b3, b4, b5, b6, b7, b8, b9, b00;
@@ -26,6 +35,8 @@ function myGame() {
     b9 = document.getElementById("b9").value;
     b00 = document.getElementById("b00").value;
 
+
+    //Player 1 win criteria
     if ((b1 == X) && (b2 == X) && (b3 == X)) {
         winAlert.innerHTML = p1Win;
         b4.disabled=true;
@@ -35,7 +46,7 @@ function myGame() {
         b8.disabled=true;
         b9.disabled=true;
         alert(p1Win);
-    };
+    }
 
     else if ((b4 == X) && (b5 == X) && (b6 == X)) {
     winAlert.innerHTML = p1Win;
@@ -46,8 +57,8 @@ function myGame() {
     b8.disabled=true;
     b9.disabled=true;
     alert(p1Win);
-    };
-/*
+    }
+
     else if ((b7 == X) && (b8 == X) && (b9 == X)) {
         winAlert.innerHTML = p1Win;
         b.disabled=true;
@@ -57,26 +68,107 @@ function myGame() {
         b.disabled=true;
         b.disabled=true;
         alert(p1Win);
-        };
+        }
 
-        else if ((b == X) && (b == X) && (b == X)) {
+    else if ((b1 == X) && (b5 == X) && (b9 == X)) {
+    winAlert.innerHTML = p1Win;
+    b2.disabled=true;
+    b3.disabled=true;
+    b4.disabled=true;
+    b6.disabled=true;
+    b7.disabled=true;
+    b8.disabled=true;
+    alert(p1Win);
+    }
+
+    else if ((b3 == X) && (b5 == X) && (b7 == X)) {
         winAlert.innerHTML = p1Win;
-        b.disabled=true;
-        b.disabled=true;
-        b.disabled=true;
-        b.disabled=true;
-        b.disabled=true;
-        b.disabled=true;
+        b1.disabled=true;
+        b2.disabled=true;
+        b4.disabled=true;
+        b6.disabled=true;
+        b8.disabled=true;
+        b9.disabled=true;
         alert(p1Win);
-        };
-}
+        }
 
+    //Player 2 win criteria
+    else if ((b1 == O) && (b2 == O) && (b3 == O)) {
+        winAlert.innerHTML = p2Win;
+        b4.disabled=true;
+        b5.disabled=true;
+        b6.disabled=true;
+        b7.disabled=true;
+        b8.disabled=true;
+        b9.disabled=true;
+        alert(p2Win);
+        }
 
+    else if ((b4 == O) && (b5 == O) && (b6 == O)) {
+        winAlert.innerHTML = p2Win;
+        b1.disabled=true;
+        b2.disabled=true;
+        b3.disabled=true;
+        b7.disabled=true;
+        b8.disabled=true;
+        b9.disabled=true;
+        alert(p2Win);
+        }
+
+    else if ((b7 == O) && (b8 == O) && (b9 == O)) {
+        winAlert.innerHTML = p2Win;
+        b1.disabled=true;
+        b2.disabled=true;
+        b3.disabled=true;
+        b4.disabled=true;
+        b5.disabled=true;
+        b6.disabled=true;
+        alert(p2Win);
+        }
+
+    else if ((b1 == O) && (b5 == O) && (b9 == O)) {
+        winAlert.innerHTML = p2Win;
+        b2.disabled=true;
+        b3.disabled=true;
+        b4.disabled=true;
+        b6.disabled=true;
+        b7.disabled=true;
+        b8.disabled=true;
+        alert(p2Win);
+        }
+
+    else if ((b3 == O) && (b5 == O) && (b7 == O)) {
+        winAlert.innerHTML = p2Win;
+        b1.disabled=true;
+        b2.disabled=true;
+        b4.disabled=true;
+        b6.disabled=true;
+        b7.disabled=true;
+        b8.disabled=true;
+        alert(p2Win);
+        }
+
+    //Tie criteria
+
+    else if ((b1 == X || b1 == O) && (b2 == X || b2 == O) && 
+    (b3 == X || b3 == O) && (b4 == X || b4 == O) && (b5 == X || b5 == O)
+    && (b6 == X || b6 == O) && (b7 == X || b7 == O) && (b8 == X || b8 == O)
+    && (b9 == X || b9 == O)) {
+        winAlert.innerHTML = aTie;
+        alert(aTie);
+    }
+
+    else {
+        if (flag = 1) {
+            winAlert.innerHTML = "Go for Player 1!";
+        }
+
+        else {
+            winAlert.innerHTML = "Go Player 2!";
+        }
+    }
+
+};
 
 
 alert("You're good to go!");
-
-
-*/
-
-};
